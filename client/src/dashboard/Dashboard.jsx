05647a714e3d7c435fddcda6ec5fefb06d420184
Pranoy Biswas <impranoybiswas@gmail.com>
2025-08-10 16:53:01 +0600

    sign out add in dashboard

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,18 +1,26 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../providers/Context";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import { Link, NavLink, Outlet } from "react-router";
 import { dashbordLinks } from "../utilities/DashbordLinks";
 
-
 import Aos from "aos";
 import HotToster from "../components/HotToster";
-import usePageTitle from "../hooks/usePageTitle";
+import { FaSignOutAlt } from "react-icons/fa";
+import ThemeToggler from "../components/ThemeToggler";
 
 export default function Dashboard() {
-    usePageTitle("Dashboard | Share Bite");
-  const { user, loading } = useContext(FirebaseContext);
+  const { user, loading, signOutUser } = useContext(FirebaseContext);
   const [isIcon, setIsIcon] = useState(false);
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: false,
+      mirror: false,
+    });
+  }, []);
 
   return (
     <>
@@ -42,14 +50,16 @@ export default function Dashboard() {
                 alt=""
               />
               <div className="flex flex-col text-white">
-                <p className="text-sm md:text-base font-semibold">{user.displayName}</p>
+                <p className="text-sm md:text-base font-semibold">
+                  {user.displayName}
+                </p>
                 <p className="text-xs">{user.email}</p>
               </div>
             </>
           )}
         </div>
       </nav>
-      <HotToster/>
+      <HotToster />
       <main className="w-full flex gap-0 pt-14">
         <div className="hidden md:flex flex-col w-auto bg-primary text-white transition-all duration-500 ease-in-out min-h-dvh">
           {dashbordLinks.map((link, index) => (
@@ -63,18 +73,42 @@ export default function Dashboard() {
                   }`
               }
             >
-              <span className={`text-sm w-14 h-10 flex justify-center items-center`}>
+              <span
+                className={`text-sm w-14 h-10 flex justify-center items-center`}
+              >
                 {link.icon}
               </span>
-              <span className={`${isIcon ? "hidden" : "flex"} pr-4 transition-all duration-500 ease-in-out`}>
+              <span
+                className={`${
+                  isIcon ? "hidden" : "flex"
+                } pr-4 transition-all duration-500 ease-in-out`}
+              >
                 {link.name}
               </span>
             </NavLink>
           ))}
+          <div className="flex justify-center items-center pt-20">
+            <ThemeToggler />
+          </div>
+          <button
+            onClick={signOutUser}
+            className="flex items-center justify-center gap-2 text-lg font-semibold transition-all duration-300 ease-in-out hover:bg-secondary py-2 m-3 rounded-full cursor-pointer"
+          >
+            <FaSignOutAlt /> <span
+                className={`${
+                  isIcon ? "hidden" : "flex"
+                } pr-4 transition-all duration-500 ease-in-out`}
+              >
+                Sign Out
+              </span>
+          </button>
         </div>
-        <div className={`absolute top-0 left-0 z-45 md:hidden w-2/3 h-dvh ${isIcon ? "flex" : "hidden"} bg-primary text-white flex flex-col pt-14`}>
-
-        {dashbordLinks.map((link, index) => (
+        <div
+          className={`absolute top-0 left-0 z-45 md:hidden w-2/3 h-dvh ${
+            isIcon ? "flex" : "hidden"
+          } bg-primary text-white flex flex-col pt-14`}
+        >
+          {dashbordLinks.map((link, index) => (
             <NavLink
               key={index}
               to={link.path}
@@ -85,18 +119,31 @@ export default function Dashboard() {
                   }`
               }
             >
-              <span className={`text-sm w-14 h-10 flex justify-center items-center`}>
+              <span
+                className={`text-sm w-14 h-10 flex justify-center items-center`}
+              >
                 {link.icon}
               </span>
-              <span className={`flex pr-4 transition-all duration-500 ease-in-out`}>
+              <span
+                className={`flex pr-4 transition-all duration-500 ease-in-out`}
+              >
                 {link.name}
               </span>
             </NavLink>
           ))}
 
+          <div className="flex justify-center items-center pt-10">
+            <ThemeToggler />
+          </div>
+          <button
+            onClick={signOutUser}
+            className="flex items-center justify-center gap-2 text-lg font-semibold transition-all duration-300 ease-in-out hover:bg-secondary py-2 m-3 rounded-full cursor-pointer"
+          >
+            <FaSignOutAlt /> Sign Out
+          </button>
         </div>
         <div className="flex-1 px-2">
-            <Outlet/>
+          <Outlet />
         </div>
       </main>
     </>
